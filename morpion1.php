2f07e234c1d4f8f47d7d3b962b3dcb61d5eed188
spadaremy <remy.spada@viacesi.fr>
2017-07-11 17:51:16 +0200

    morpion

---
+++ morpion1.php
@@ -0,0 +1,33 @@
+
+ 
+<?php
+include ("tictactoe.php");
+?>
+ 
+ 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    <table border="1" width="220" height="220" cellspacing="0" cellpadding="0">
+    <tr>
+        <td  id="cell1" > </td>
+        <td  id="cell2" ></td>
+        <td id="cell3" ></td>
+    </tr>
+    <tr>
+        <td  id="cell4" ></td>
+        <td  id="cell5" ></td>
+        <td  id="cell6" ></td>
+    </tr>
+    <tr>
+        <td   id="cell7" ></td>
+        <td   id="cell8" ></td>
+        <td  id="cell9"></td>
+    </tr>
+</table>
+    </body>
+</html>
